5af7690eb4953a4487da28d5cccac7c8c44fce32
Daniele Paz <danielepaz404@gmail.com>
2025-07-29 22:51:07 -0300

    :recycle: Refactor to isolate tests by feature and add custom command

--- cypress/e2e/base.cy.js
+++ cypress/e2e/base.cy.js
@@ -1,46 +1,31 @@
 /// <reference types="cypress" />
 
-describe('base tests', () => {
-    beforeEach(() => {
-    cy.intercept(
-        {
-        url: 'https://magento.softwaretestingboard.com/**',
-        middleware: true,
-        },
-        (req) => {
-        req.on('response', (res) => {
-            // Throttle the response to 1 Mbps to simulate a
-            // mobile 3G connection
-            res.setThrottle(100)
-        })
-        }
-    )
-    })
-
+describe('home page', () => {
     it('should load the page with no errors', () => {
-        cy.request('https://magento.softwaretestingboard.com/')
-        .should((response) => {
+        cy.request('/').should((response) => {
             expect(response.status).to.eq(200)
         });
     })
+});
 
-    it('should search for shirt',  {
-      retries: {
-        runMode: 2,
-        openMode: 2,
-      },
-    }, () => {
-        cy.intercept('https://magento.softwaretestingboard.com/catalogsearch/result/**').as('results');
-        cy.visit('https://magento.softwaretestingboard.com/')
-        cy.get('#search').type('shirt{enter}')
-        
+describe('search', () => {
+    it('should redirect to results page when search for shirt', () => {
+        cy.searchForTerm('shirt');
+
         cy.location().should(loc => {
-            expect(loc.toString()).to.eq('https://magento.softwaretestingboard.com/catalogsearch/result/?q=shirt');
+            expect(loc.toString()).to.eq(`${Cypress.config('baseUrl')}/catalogsearch/result/?q=shirt`);
         });
-        cy.wait('@results');
+
+        cy.get('.results').should('be.visible');
+    })
+});
+
+describe('add to cart', () => {
+    it('should add last product from result page to cart', () => {
+        cy.visit('/');
+        cy.searchForTerm('shirt');
 
         cy.get('.search.results .product-items .product-item').last().then(lastProductItem => {
-
             lastProductItem.find('.swatch-attribute.size .swatch-option').first().trigger('click');
             lastProductItem.find('.swatch-attribute.color .swatch-option').first().trigger('click');
 
@@ -50,7 +35,5 @@ describe('base tests', () => {
         cy.get("[data-block='minicart']").find('.counter-number').should(count => {
             expect(count.text()).to.equal('1');
         });
-
-
     })
 })
