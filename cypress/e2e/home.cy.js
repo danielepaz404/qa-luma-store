878bf62bb7af352e36accade00efdd2e038d18df
Daniele Paz <danielepaz404@gmail.com>
2025-07-31 04:58:55 -0300

    :fire: Remove unecessary it

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,7 @@
 describe('Home page', () => {
-    it('loads successfuly', () => {
-        it('should load the page with no errors', () => {
-            cy.request('/').should((response) => {
-                expect(response.status).to.eq(200)
-            });
-        })
+    it('should load the page with no errors', () => {
+        cy.request('/').should((response) => {
+            expect(response.status).to.eq(200)
+        });
     });
 })
